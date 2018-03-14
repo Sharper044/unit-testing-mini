7e1bbafec8bfcb1bcb63c5cb6ef52cca576a1318
Stuart Harper <stuartharper044@gmail.com>
2018-03-14 13:26:04 -0600

    asignment complete

---
+++ functions.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+  returnTwo: () => {
+    return 2;
+  },
+
+  greeting: (name) => {
+    return `Hello, ${name}.`;
+  },
+
+  add: (x, y) => {
+    return x + y;
+  },
+
+  multiply: (x, y) => {
+    return x * y;
+  },
+
+  divide: (x, y) => {
+    return x / y;
+  },
+
+  subtract: (x, y) => {
+    return x - y;
+  }
+}
